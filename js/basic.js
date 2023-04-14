a982ee0dd479ba3e7997c9391a6a1137a66b5f9e
LHN <msrim2007@gmail.com>
2023-04-14 16:22:15 +0900

    wasdExtend style edit

--- js/basic.js
+++ js/basic.js
@@ -1,9 +1,3 @@
 console.log('  /\\_/\\');
 console.log(' ( o.o )');
 console.log('  > ^ <');
-
-try {
-	setTimeout(() => { throw new Error('Error!'); }, 1000);
-} catch(e) {
-	console.error('HI , ' + e);
-}
