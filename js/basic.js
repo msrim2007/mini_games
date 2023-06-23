252c646e0d623ec5283ec1350f55d90cf95a1e17
LHN <msrim2007@gmail.com>
2023-06-23 13:30:19 +0900

    basic js debounce test

--- js/basic.js
+++ js/basic.js
@@ -19,3 +19,31 @@ console.log(
   > ^ <
   `
 );
+
+
+
+
+
+/*
+ * DEBOUNCE TEST
+*/
+function btnClickEvent() {
+  console.log('click');
+}
+
+function debounce(callBackFunc) {
+  let timer;
+
+  return (...args) => {
+    if (!timer) callBackFunc.apply(this, args);
+    clearTimeout(timer);
+    timer = setTimeout(() => { timer = undefined; } , 1000);
+  };
+}
+
+const $BTN = document.createElement('input');
+$BTN.type = 'button';
+$BTN.id = 'btn';
+document.querySelector('.container').appendChild($BTN);
+document.getElementById('btn').addEventListener('click', debounce(() => btnClickEvent()));
+
